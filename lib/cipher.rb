c661f68fadceb89612a32ed67ca716a03617ac3c
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-17 14:31:11 -0600

    Pass test_it_can_create_a_character_set

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,2 +1,6 @@
 class Cipher
+  attr_reader :char_set
+  def initialize
+    @char_set = ('a'..'z').to_a << ' '
+  end
 end
--- test/cipher_test.rb
+++ test/cipher_test.rb
@@ -9,4 +9,11 @@ class CipherTest < Minitest::Test
   def test_it_exists
     assert_instance_of Cipher, @cipher
   end
+
+  def test_it_can_create_a_character_set
+    character_set = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+                  'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+
+    assert_equal character_set, @cipher.char_set
+  end
 end
