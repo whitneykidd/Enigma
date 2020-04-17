252b6b4259982164c15722b08d72d3c398795e8c
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-17 15:16:33 -0600

    Pass test_it_can_generate_key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,3 +1,7 @@
 require './lib/cipher'
+
 class Enigma < Cipher
+  def generate_key
+    rand(99999).to_s.rjust(5, '0')
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -10,4 +10,9 @@ class EnigmaTest < Minitest::Test
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_it_can_generate_key
+    @enigma.stubs(:rand).returns('2715')
+    assert_equal '02715', @enigma.generate_key
+  end
 end
