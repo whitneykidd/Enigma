e964aa27cd89ba261860b5bd1c52ace9065f1a1c
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-18 15:37:08 -0600

    Update encrypt method and test to return hash

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -4,6 +4,10 @@ class Enigma < Cipher
   def encrypt(message, key, date)
     shifts = generate_shifts(key, date, 1)
     mutate_message(message, shifts)
+
+    {encryption: mutate_message(message, shifts),
+     key: key,
+     date: date}
   end
 
   def decrypt(cipher_text, key, date)
